56773e10068d0267094b441204817170b03e6eb3
Matheus Robert <roberttjmelo@hotmail.com>
2020-05-24 11:17:44 -0300

    Criacao

---
+++ src/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import Home from './screens/Home'
+import Wallet from './screens/Wallet'
+import Pay from './screens/Pay' 
+
+
+
